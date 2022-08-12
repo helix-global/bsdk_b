b9f5754af33aba65414bac95306788b75a2e68d5
maystrenco <maystrenco@ro.ru>
2022-08-12 20:04:33 +0300

    {bsdev}

---
+++ src/dotnet/assemblies/BinaryStudio.VSShellIsolatedPackage.2015/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace BinaryStudio.VSShellIsolatedPackage2015.AboutBoxPackage
+{
+    static class GuidList
+    {
+        public const string guidAboutBoxPackagePkgString = "52891e72-35f2-446c-8cfc-4bcb4c417ba6";
+        public const string guidAboutBoxPackageCmdSetString = "6187d09a-aae7-4f8e-8326-5a25a77adbf1";
+
+        public static readonly Guid guidAboutBoxPackageCmdSet = new Guid(guidAboutBoxPackageCmdSetString);
+    };
+}
---
+++ src/dotnet/assemblies/BinaryStudio.VSShellIsolatedPackage.2015/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace BinaryStudio.VSShellIsolatedPackage2015.AboutBoxPackage
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidHelpAbout = 0x100;
+
+
+    };
+}
