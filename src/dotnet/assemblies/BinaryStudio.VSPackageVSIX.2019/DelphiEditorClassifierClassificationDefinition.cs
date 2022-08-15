7c802e5809409685d291461606f255444fa4ddce
maystrenco <maystrenco@ro.ru>
2022-08-15 14:01:55 +0300

    {vs}

---
+++ src/dotnet/assemblies/BinaryStudio.VSPackageVSIX.2019/DelphiEditorClassifierClassificationDefinition.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace BinaryStudio.VSPackageVSIX
+{
+    /// <summary>
+    /// Classification type definition export for DelphiEditorClassifier
+    /// </summary>
+    internal static class DelphiEditorClassifierClassificationDefinition
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        /// <summary>
+        /// Defines the "DelphiEditorClassifier" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("DelphiEditorClassifier")]
+        private static ClassificationTypeDefinition typeDefinition;
+
+#pragma warning restore 169
+    }
+}
---
+++ src/dotnet/assemblies/BinaryStudio.VSPackageVSIX.2019/DelphiEditorClassifierFormat.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+
+namespace BinaryStudio.VSPackageVSIX
+{
+    /// <summary>
+    /// Defines an editor format for the DelphiEditorClassifier type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "DelphiEditorClassifier")]
+    [Name("DelphiEditorClassifier")]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    internal sealed class DelphiEditorClassifierFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelphiEditorClassifierFormat"/> class.
+        /// </summary>
+        public DelphiEditorClassifierFormat()
+        {
+            this.DisplayName = "DelphiEditorClassifier"; // Human readable version of the name
+            this.BackgroundColor = Colors.BlueViolet;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+}
